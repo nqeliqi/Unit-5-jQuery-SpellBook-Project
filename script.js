4afc584905be0307a678499e6bda28d97a6b852c
nqeliqi <43378819+nqeliqi@users.noreply.github.com>
2019-01-08 09:46:58 -0500

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,23 @@
+//Spell #1
+
+$("#Click").click(function(){
+    $("#ball").fadeToggle();
+ 
+});
+
+
+
+//Spell #2
+
+$("#Button").click(function(){
+    $("#fire").toggle();
+ 
+});
+
+
+
+//Spell #3
+
+$("#thing").click(function(){
+   $("#water").slideToggle();
+});
